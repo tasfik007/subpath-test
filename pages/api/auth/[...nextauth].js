8efe8a977b58cbf92c07b493c1947a5e861611ae
Tasfik Rahman <tasfik.rahman@dsinnovators.com>
2023-06-11 17:49:39 +0600

    Include keycloak as auth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,6 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import {keycloakProviderConfig} from "./keycloakProviderConfig";
+import {githubProviderConfig} from "./githubProviderConfig";
 
-const options = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-    ],
-};
-
-export default (req, res) => NextAuth(req, res, options);
+// export default NextAuth(githubProviderConfig);
+export default NextAuth(keycloakProviderConfig);
